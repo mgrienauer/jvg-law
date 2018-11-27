cb8965996f372c328bc9a7d38703f3c4adbca641
mgrienauer@gmail.com <mgrienauer@gmail.com>
2018-11-27 15:49:17 +0100

    Update top info section

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import TopNav from './components/Topnav';
 import TopJumbo from './components/TopJumbo'
 
+const appWidth = {
+  maxWidth: '1020px',
+  width: '100%',
+  margin: 'auto'
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={appWidth}>
         <TopJumbo />
         <TopNav/>
       </div>
--- src/components/TopJumbo.js
+++ src/components/TopJumbo.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 
+const topJumboStyle = {
+  height: '200px',
+  alignItems: 'center',
+  display: 'flex',
+  backgroundImage: 'linear-gradient(to right, white , #a8f3ff, white )'
+};
+
 const TopJumbo = (props) => {
   return (
-    <div>
+    <div style={topJumboStyle}>
       <Container fluid className="top-jumbo">
-        <h1 className="">John V. Grienauer, Esq.</h1>
-        <div class="">
-          <p className="lead">Call Now:</p>
-          <p>310-123-4567</p>
+        <div className="float-left">
+          <h1>John V. Grienauer, Esq.</h1>
+          <p>Serving the Los Angeles Area</p>
+        </div>
+        <div class="float-right">
+          <h2>Call for a free consulatation:</h2>
+          <h2 className="text-center">310-123-4567</h2>
         </div>
       </Container>
     </div>
