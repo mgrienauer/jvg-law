a818a9e1063e4c25a25ccfc6cec987b64c7cd8c9
Michael Grienauer <mgrienauer@gmail.com>
2019-04-06 22:27:37 +0200

    Add icon and change site title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,18 @@ import { Container, Row, Col } from 'reactstrap';
 const footerStyle = {
   height: '150px',
   alignItems: 'center',
-  margin: '0'
+  margin: '0',
+  marginBottom: '0',
+  padding: '20px',
+  position: 'absolute',
+  width: '100%'
 };
 
 export default function Footer(props) {
   return (
-    <footer className="footer bg-dark mt-5 mb-0">
+    <footer style={footerStyle} className="footer bg-dark mt-5">
       <Container style={props.appWidth}>
-        <Row style={footerStyle}>
+        <Row>
           <Col>
             <p className="text-white text-center">
               John Vincent Grienauer, Esq. &nbsp;•&nbsp; 310-373-5944 &nbsp;•&nbsp; john.grienauer@cox.net
