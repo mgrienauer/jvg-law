76f4f39bc5a34fb466f03b8d9471135a19e37c4b
Michael Grienauer <mgrienauer@gmail.com>
2019-04-14 20:07:46 +0200

    Add headshot

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap';  
-import Video from './Video'    
+import Video from './Video'   
+import Headshot from './Headshot' 
 
 export default function Intro(props) {
   return (
     <Container>
         <Row className="mt-3" id="intro">
-            <Col lg="6" sm="12">
+            <Col lg={{ size: 7, offset: 1 }} sm="12">
                 <Video />
+            </Col>
+            <Col lg="4" sm="12" className="mt-4 d-flex justify-content-center">
+              <Headshot />
             </Col>
         </Row>
     </Container>
