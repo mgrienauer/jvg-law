409da7252b72ea84c74bdb42ca71ff18d11a2668
Michael Grienauer <mgrienauer@gmail.com>
2020-04-24 13:49:44 -0700

    update contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,34 +1,69 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import React from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
 const formStyling = {
-  width: '100%',
-  padding: '15px',
-  border: '1px solid #ced4da',
-  borderRadius: '5px'
-}
+	width: "100%",
+	padding: "15px",
+	border: "1px solid #ced4da",
+	borderRadius: "5px",
+};
 
 export default class Example extends React.Component {
-  render() {
-    return (
-      <Form method="POST" action="https://formspree.io/john.grienauer@cox.net" className="mt-5" id="contact" style={formStyling}>
-        <h2 className="text-center">Contact Us</h2>
-        <p>Fill out the contact form or send an email to: <strong>john.grienauer@cox.net</strong></p>
-        <FormGroup>
-          <Label for="exampleName">Name</Label>
-          <Input type="text" name="name" id="exampleName" placeholder="Enter your full name" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleText">Email</Label>
-          <Input type="email" name="_replyto" id="exampleEmail" placeholder="Enter your email" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleEmail">Your Message</Label>
-          <Input type="textarea" name="message" id="exampleMessage" placeholder="Enter a brief description of your legal issue" />
-        </FormGroup>
-        <Button type="submit">Send Inquiry</Button>
-        <input type="hidden" name="_next" value="www.johngrienauerlaw.com" />
-      </Form>
-    );
-  }
+	render() {
+		return (
+			<>
+				<form name="contact" netlify netlify-honeypot="bot-field" hidden>
+					<input type="text" name="name" />
+					<input type="email" name="email" />
+					<textarea name="message" />
+				</form>
+
+				<Form
+					name="contact"
+					method="POST"
+					data-netlify="true"
+					className="mt-5"
+					id="contact"
+					style={formStyling}
+				>
+					<input type="hidden" name="form-name" value="contact" />
+					<h2 className="text-center">Contact Us</h2>
+					<p>
+						Fill out the contact form or send an email to:{" "}
+						<strong>john.grienauer@cox.net</strong>
+					</p>
+					<FormGroup>
+						<Label for="exampleName">Name</Label>
+						<Input
+							type="text"
+							name="name"
+							id="exampleName"
+							placeholder="Enter your full name"
+						/>
+					</FormGroup>
+					<FormGroup>
+						<Label for="exampleText">Email</Label>
+						<Input
+							type="email"
+							name="email"
+							id="exampleEmail"
+							placeholder="Enter your email"
+						/>
+					</FormGroup>
+					<FormGroup>
+						<Label for="exampleEmail">Your Message</Label>
+						<Input
+							type="textarea"
+							name="message"
+							id="exampleMessage"
+							placeholder="Enter a brief description of your legal issue"
+						/>
+					</FormGroup>
+					<Button type="submit" color="primary">
+						Send Inquiry
+					</Button>
+				</Form>
+			</>
+		);
+	}
 }
